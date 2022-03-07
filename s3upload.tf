d25a83e1936399d64dcdb2d5950ad8176f124e1a
nrekapalli189 <99802972+nrekapalli189@users.noreply.github.com>
2022-03-07 07:08:44 +0530

    Update s3upload.tf

--- s3upload.tf
+++ s3upload.tf
@@ -1,6 +1,16 @@
-resource "aws_s3_bucket" "my_bucket" {
+  resource "aws_s3_bucket" "s3bucket" {
   bucket = "naga111"
 
-
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.s3bucket.id
+  acl    = "private"
+}
+
+
 
