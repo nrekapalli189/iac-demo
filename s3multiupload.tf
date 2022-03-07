920a66a7eaee01b0e2cc17f17f9d3c9a6bb5f1a6
nrekapalli189 <99802972+nrekapalli189@users.noreply.github.com>
2022-03-07 15:02:42 +0530

    Update s3multiupload.tf

--- s3multiupload.tf
+++ s3multiupload.tf
@@ -1,13 +1,13 @@
-resource "aws_s3_bucket_object" "object" {
-  bucket = "naga101"
-  key    = "vars.tf"
-  source = "${path.module}/vars.tf"
-  etag   = "${filemd5("${path.module}/vars.tf")}"
+resource "aws_s3_bucket" "b" {
+  bucket = "naga8593"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
 
-resource "aws_s3_bucket_object" "object1" {
-  bucket = "naga101"
-  key    = "instance.tf"
-  source = "${path.module}/instance.tf"
-  etag   = "${filemd5("${path.module}/instance.tf")}"
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.b.id
+  acl    = "private"
 }
