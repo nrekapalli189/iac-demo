58fb43159bef3f63a6e76802c0374e4140ce56b7
nrekapalli189 <99802972+nrekapalli189@users.noreply.github.com>
2022-03-07 06:58:03 +0530

    Update s3upload.tf

--- s3upload.tf
+++ s3upload.tf
@@ -1,13 +1,14 @@
-resource "aws_s3_bucket_object" "object" {
-  bucket = "naga101"
-  key    = "jenkins.war"
-  source = "/root/jenkins.war"
-  etag   = "${filemd5("${/root}/jenkins.war")}"
+
+resource "aws_s3_bucket" "s3bucket" {
+  bucket = "naga111"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
 
-resource "aws_s3_bucket_object" "object1" {
-  bucket = "naga101"
-  key    = "file.txt"
-  source = "/root/file.txt"
-  etag   = "${filemd5("${/root}/file.txt")}"
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.s3bucket.id
+  acl    = "private"
 }
