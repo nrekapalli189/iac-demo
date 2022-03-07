21b1edddf696925bdd1ca6dc0063e25124c0ec8c
nrekapalli189 <99802972+nrekapalli189@users.noreply.github.com>
2022-03-07 06:53:55 +0530

    Update s3upload.tf

--- s3upload.tf
+++ s3upload.tf
@@ -1,13 +1,13 @@
 resource "aws_s3_bucket_object" "object" {
   bucket = "naga101"
   key    = "jenkins.war"
-  source = "${path.module}/jenkins.war"
-  etag   = "${filemd5("${path.module}/jenkins.war")}"
+  source = "/root/jenkins.war"
+  etag   = "${filemd5("${/root}/jenkins.war")}"
 }
 
 resource "aws_s3_bucket_object" "object1" {
   bucket = "naga101"
   key    = "file.txt"
-  source = "${path.module}/file.txt"
-  etag   = "${filemd5("${path.module}/file.txt")}"
+  source = "/root/file.txt"
+  etag   = "${filemd5("${/root}/file.txt")}"
 }
